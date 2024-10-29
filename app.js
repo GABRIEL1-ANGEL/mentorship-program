5179873fa6f7eddc336a5f58d7c810405c740d18
Onwuka ekene <onwukadavid732@gmail.com>
2024-10-29 02:30:13 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+document.querySelector("button").addEventListener("click", trick);
+
+function trick () {
+    prompt("Are you sure?");
+    alert("You won't evade paying the mentor money");
+    prompt("Are you really sure this is the last time we'll ask?");
+    alert("Request made!");
+}
+
+
